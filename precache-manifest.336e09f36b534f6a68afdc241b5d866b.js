55d2b8972f304663b542f20fed3ae85a0ab256b5
Ajay Jat <jatajay004@gmail.com>
2020-06-30 15:50:18 +0530

    Updates

---
+++ precache-manifest.336e09f36b534f6a68afdc241b5d866b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5060123080349dd5bee4c14f47172c",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/index.html"
+  },
+  {
+    "revision": "6ba620ac38c2875580e4",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7d140e18b6f39f19e077",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/css/main.3d735ec3.chunk.css"
+  },
+  {
+    "revision": "6ba620ac38c2875580e4",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/js/2.fcaccd92.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/js/2.fcaccd92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d140e18b6f39f19e077",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/js/main.e7ac39bd.chunk.js"
+  },
+  {
+    "revision": "d3691495e49217a84b49",
+    "url": "/ajay-jat214/E-Commerce-Mobile-App.git/static/js/runtime-main.b5de2787.js"
+  }
+]);
